c45090999d96c17c17db9311b33a9a17b3a241cc
amirmtolouie@gmail.com <amirmtolouie@gmail.com>
2023-06-17 09:33:05 +0300

    Background Gradient Done

--- CodeQuotes/MainPage.xaml.cs
+++ CodeQuotes/MainPage.xaml.cs
@@ -9,6 +9,33 @@ public partial class MainPage : ContentPage
 
     private void btnGenerateQuote_Clicked(object sender, EventArgs e)
     {
+		var rnd = new Random();
+
+		var startColor = System.Drawing.Color.FromArgb(rnd.Next(0,256), rnd.Next(0, 256), rnd.Next(0, 256));
+
+		var EndColor = System.Drawing.Color.FromArgb(rnd.Next(0,256), rnd.Next(0, 256), rnd.Next(0, 256));
+
+
+		var colors = ColorUtility.ColorControls.GetColorGradient(startColor,EndColor,6);
+
+        float stopOffset = .0f;
+        var stops = new GradientStopCollection();
+        foreach (var c in colors)
+        {
+            stops.Add(new GradientStop(Color.FromArgb(c.Name),
+                 stopOffset));
+            stopOffset += .2f;
+        }
+
+        var gradient =
+             new LinearGradientBrush(stops,
+                  new Point(0, 0),
+                  new Point(1, 1));
+
+        background.Background = gradient;
+
+
+
 
     }
 }
