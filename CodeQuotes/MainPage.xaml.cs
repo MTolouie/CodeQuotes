396f0e3d651230583e719199a6d0167146f43358
amirmtolouie@gmail.com <amirmtolouie@gmail.com>
2023-06-17 09:04:56 +0300

    Add project files.

---
+++ CodeQuotes/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace CodeQuotes;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void btnGenerateQuote_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
+
